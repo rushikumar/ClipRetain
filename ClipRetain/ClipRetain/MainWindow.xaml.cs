004c4e6a666868f6f05394b1928af4a65a0dba15
Rushikumar <rushikumar.bhatt+GitHub+WPF+ClipRetain@gmail.com>
2017-12-15 00:01:29 -0500

    Initial commit

---
+++ ClipRetain/ClipRetain/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClipRetain
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PasteOnLoad();
+        }
+
+        public void PasteOnLoad()
+        {
+            IDataObject clipBoardData = Clipboard.GetDataObject();
+            crCurrentContent.Content = Clipboard.GetText();
+            //crCurrentContent.Document = new FlowDocument(new Paragraph(new Run(Clipboard.GetDataObject)));
+
+            var cntr = 1;
+            //var newHistoryListItem = new ListViewItem(Clipboard.GetText());
+            crClipHistoryList.Items.Add(cntr.ToString() + " - " + Clipboard.GetText()); cntr++;
+            crClipHistoryList.Items.Add(cntr.ToString() + " - " + Clipboard.GetText()); cntr++;
+            crClipHistoryList.Items.Add(cntr.ToString() + " - " + Clipboard.GetText()); cntr++;
+            crClipHistoryList.Items.Add(cntr.ToString() + " - " + Clipboard.GetText()); cntr++;
+            crClipHistoryList.Items.Add(cntr.ToString() + " - " + Clipboard.GetText()); cntr++;
+            crClipHistoryList.Items.Add(cntr.ToString() + " - " + Clipboard.GetText()); cntr++;
+
+            var totalCharacters = 0;
+            totalCharacters = Clipboard.GetText().Count();
+
+            crCurrentStats.Content = "Stats: Total Characters: " + totalCharacters;
+
+            return;
+        }
+
+        private void Button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            if (crClipHistoryList.SelectedIndex < 0)
+            {
+                MessageBox.Show("No item selected!", "Select Item First!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                Clipboard.SetText(crClipHistoryList.SelectedItem.ToString());
+            }
+        }
+
+        private void Button_Remove_Click(object sender, RoutedEventArgs e)
+        {
+            if (crClipHistoryList.SelectedIndex < 0)
+            {
+                //ShowDialog("Select an item first!");
+                MessageBox.Show("No item selected!", "Select Item First!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                crClipHistoryList.Items.RemoveAt(crClipHistoryList.SelectedIndex);
+            }
+            /*foreach(ListViewItem item in crClipHistoryList.SelectedItems)
+            {
+                crClipHistoryList.Items.Remove(item);
+            }*/
+        }
+
+        private void Button_Remove_All_Click(object sender, RoutedEventArgs e)
+        {
+            crClipHistoryList.Items.Clear();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ClipRetain
+## Description
 Clipboard (copy history) retainer/manager
+
+## Motivation
+I haven't touched C# in a LONG time; the last I major (i.e. publicly released) project worked on a C# project was eons ago, on a project called "RoboDown" https://sourceforge.net/projects/rapiddown/, in 2009. I have been itching to get into C# again and what better way than to start with a WPF application?
+
+Right off bat, there were two goals to achieve by creating this project:
+ 1. Get practical experience in C#
+ 2. Create something meaningful, while at it
+
+## Initial work (v1.0 goals)
+1. Text only operations/abilities for this tool
+
+## ToDo (in no particular order)
+1. Capability of swapping contents
+   - i.e. item #4 with #8 etc., in the ClipRetain history list
+2. Removal of multiple items from the ClipRetain history list
